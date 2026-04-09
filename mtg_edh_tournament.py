90f2119f749a98a0c11e575f15f6ab1f06a4444f
blamb104 <bryanlamb104@gmail.com>
2026-04-09 16:33:29 -0500

    Update mtg_edh_tournament.py

--- mtg_edh_tournament.py
+++ mtg_edh_tournament.py
@@ -19,7 +19,7 @@ st.markdown("""
     }
     /* Main Landing Header styling */
     .landing-header {
-        font-size: 8rem;
+        font-size: 4rem !important;
         font-weight: 800;
         text-align: center;
         margin-bottom: 0px;
